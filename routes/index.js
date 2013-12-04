674619e2c183eefff5464a278cca079e8cce9270
rakeLL <satbirdd@gmail.com>
2013-12-04 12:52:48 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 exports.index = function(req, res){
   var projects =
     [ 'mongod',
-      'redis-commander'
+      'redis-commander',
       'elasticsearch',
       'Caramel-Server-Backends',
       'Caramel-Server',
