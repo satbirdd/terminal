2fd7110b98b7001958fa21ee914248310ccdc706
rakeLL <satbirdd@gmail.com>
2013-12-07 09:46:16 +0800

    arthor color

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@
 exports.index = function(req, res){
   var projects =
     [ 
-      // 'mongod',
-      // 'redis-commander',
-      // 'elasticsearch',
-      // 'Caramel-Server-Backends',
-      // 'Caramel-Server',
-      'todo',
-      'coolcloth'
-      // 'Accounts',
-      // 'Panama'
+      'mongod',
+      'redis-commander',
+      'elasticsearch',
+      'Caramel-Server-Backends',
+      'Caramel-Server',
+      // 'todo',
+      // 'coolcloth'
+      'Accounts',
+      'Panama'
     ]
   res.render('index', { title: 'Terminal', projects: projects });
 };
